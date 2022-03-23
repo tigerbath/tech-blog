9d4842cc16605b3503e0acae7c7439bdd0cbc6a7
Matthew Palmer <mattjpalmer92@gmail.com>
2022-03-23 10:33:35 +0000

    completed with suraj

---
+++ src/helpers/index.js
@@ -0,0 +1,11 @@
+const moment = require("moment");
+
+const formatDate = (date) => {
+  return moment(date).format("Do MMM, YYYY HH:mm");
+};
+
+const helpers = {
+  formatDate,
+};
+
+module.exports = helpers;
